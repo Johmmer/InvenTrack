ffe35880aac9f3f2ade0f19b97f261600119cdc3
Johmmer <johmmer300@gmail.com>
2025-11-20 15:27:49 -0400

    admin

--- sistema_inventario/inventario_app/admin.py
+++ sistema_inventario/inventario_app/admin.py
@@ -5,7 +5,19 @@ admin.site.register(Producto)
 admin.site.register(MovimientoInventario)
 admin.site.register(Categoria)
 
-@admin.register(Producto)
-
 
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'codigo', 'categoria', 'cantidad_stock')
+    search_fields = ('nombre', 'codigo')
+    list_filter = ('categoria',)
+
+class MovimientoInventarioAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'tipo_movimiento', 'cantidad', 'usuario', 'fecha')
+    search_fields = ('producto__nombre', 'producto__codigo', 'usuario__username')
+    list_filter = ('tipo_movimiento', 'fecha')
+    
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
