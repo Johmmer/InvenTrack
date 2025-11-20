5daaccdeae8574a4a439942ac19688e43ec2df00
Johmmer <johmmer300@gmail.com>
2025-11-20 15:24:46 -0400

    admin

--- sistema_inventario/inventario_app/admin.py
+++ sistema_inventario/inventario_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Producto, MovimientoInventario
+from .models import Producto, MovimientoInventario, Categoria
 
 admin.site.register(Producto)
 admin.site.register(MovimientoInventario)
+admin.site.register(Categoria)
+
+@admin.register(Producto)
 
 
 # Register your models here.
